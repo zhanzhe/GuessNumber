110373a02dd554c0ab6cdd4e3e3bc33a2f16f12a
jason <zhangzhe1103@126.com>
2015-02-05 14:10:06 +0800

    complete the question2

--- java/maven/src/main/java/AnswerGenerator.java
+++ java/maven/src/main/java/AnswerGenerator.java
@@ -1,47 +1,44 @@
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by jason on 2/2/15.
  */
 public class AnswerGenerator {
-    Random random;
+    private final List<String> historyGeneratedStrings;
+    private Random random;
+
     public AnswerGenerator(Random random) {
         this.random = random;
+        this.historyGeneratedStrings = new ArrayList<String>();
     }
 
-    public int generateNumber(){
-        Random random = new Random();
-        int number = random.nextInt(10);
-        number = ((number == 0)? 1:number);
+    public String generateNumber(){
+        String result = generateNumberOnce();
 
-        int[] numberArray = {0,0,0,0};
-        numberArray[0] = number;
-        int i = 1;
-        while(i < 4){
-            number = random.nextInt(10);
-            int j = 0;
-            for(; j<4; j++){
-                if(numberArray[j] == number)
-                   break;
-            }
-            if(j == 4){
-                numberArray[i++] = number;
-            }
+        while(historyGeneratedStrings.contains(result)){
+            result = generateNumberOnce();
         }
-        return getNumberFromBitArray(numberArray);
+
+        if(historyGeneratedStrings.size() == 2){
+            historyGeneratedStrings.remove(0);
+        }
+
+        historyGeneratedStrings.add(result);
+
+        return result;
+
     }
 
     private String generateNumberOnce(){
-        StringBuffer sb  = new StringBuffer();
-
-    }
-    private int getNumberFromBitArray(int[] bitArray){
-        int number = bitArray[0];
-        for(int i=1; i<4; i++){
-            number *= 10;
-            number += bitArray[i];
+        StringBuilder result = new StringBuilder();
+        while(result.length() < 4){
+            String digit = String.valueOf(random.nextInt(10));
+            if(result.indexOf(digit) == -1){
+                result.append(digit);
+            }
         }
 
-        return number;
+        return result.toString();
     }
+
 }
--- java/maven/src/test/java/AnswerGeneratorTest.java
+++ java/maven/src/test/java/AnswerGeneratorTest.java
@@ -2,12 +2,8 @@
  * Created by jason on 2/2/15.
  */
 import org.junit.Test;
-
-
 import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
-
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.BDDMockito.given;
@@ -17,14 +13,14 @@ public class AnswerGeneratorTest {
 
     @Test
     public void should_return_four_bit_number(){
-        String result = new AnswerGenerator().generateNumber();
+        String result = new AnswerGenerator(new Random()).generateNumber();
 
         assertThat(result.length()).isEqualTo(4);
     }
 
     @Test
     public void should_generate_number(){
-        String result  = new AnswerGenerator().generateNumber();
+        String result  = new AnswerGenerator(new Random()).generateNumber();
 
         try {
             Integer.parseInt(result);
@@ -34,22 +30,23 @@ public class AnswerGeneratorTest {
     }
 
     @Test
-    public void should_genenrate_no_duplicate_number(){
-        String result = new AnswerGenerator().generateNumber();
+    public void should_generate_no_duplicate_number(){
+        String result = new AnswerGenerator(new Random()).generateNumber();
 
         for(int i = 0; i < result.length(); i++){
             assertThat(result.indexOf(result.charAt(i))).isEqualTo(result.lastIndexOf((result.charAt(i))));
         }
     }
 
+
     @Test
-    public void should_be_random_number(){
+    public void should_not_repeat_in_three_times(){
 
         Random random = mock(Random.class);
         HashSet<String> numberSet = new HashSet<String>();
         AnswerGenerator answerGenerator = new AnswerGenerator(random);
 
-        given(random.nextInt())
+        given(random.nextInt(10))
                 .willReturn(1,2,3,4)
                 .willReturn(1,2,3,4)
                 .willReturn(2,3,4,5)
@@ -58,22 +55,30 @@ public class AnswerGeneratorTest {
         numberSet.add(answerGenerator.generateNumber());
         numberSet.add(answerGenerator.generateNumber());
         numberSet.add(answerGenerator.generateNumber());
-        numberSet.add(answerGenerator.generateNumber());
+
 
         assertThat(numberSet.size()).isEqualTo(3);
 
-
-
-
-
-
-
-
-
-        System.out.println(firstNumber + " -----  " + secondNumber);
-
-        assertThat(firstNumber).isNotEqualTo(secondNumber);
     }
 
+    @Test
+    public void should_result_repeatable_after_3_times(){
+        Random random = mock(Random.class);
+        HashSet<String> numberSet = new HashSet<String>();
+        AnswerGenerator answerGenerator = new AnswerGenerator(random);
+
+        given(random.nextInt(10))
+                .willReturn(1,2,3,4)
+                .willReturn(1,2,5,6)
+                .willReturn(2,3,4,5)
+                .willReturn(1,2,3,4);
+
+        numberSet.add(answerGenerator.generateNumber());
+        numberSet.add(answerGenerator.generateNumber());
+        numberSet.add(answerGenerator.generateNumber());
+        numberSet.add(answerGenerator.generateNumber());
+
+        assertThat(numberSet.size()).isEqualTo(3);
+    }
 
 }
